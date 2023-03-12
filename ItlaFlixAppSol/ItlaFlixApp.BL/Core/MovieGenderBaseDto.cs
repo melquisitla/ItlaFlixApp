951ec8d1ff03eed7542068b19aad5cc6bf868c69
Adrian <dejesuseusebio21@hotmail.com>
2023-03-12 14:08:42 -0400

    creando abstraccion de dto

--- ItlaFlixAppSol/ItlaFlixApp.BL/Core/MovieGenderBaseDto.cs
+++ ItlaFlixAppSol/ItlaFlixApp.BL/Core/MovieGenderBaseDto.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace ItlaFlixApp.BL.Core
 {
-    public class MovieGenderBaseDto
+    public abstract class MovieGenderBaseDto
     {
         public int Cod_genero { get; set; }
         public int Cod_pelicula { get; set; }
